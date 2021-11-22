e1312d566947caaca87eae1837b81617d4a66e03
elenamoglan <moglan.elena@gmail.com>
2021-11-22 09:52:29 +0200

    Update Problema 5 p 77.py

--- Problema 5 p 77.py
+++ Problema 5 p 77.py
@@ -1,4 +1,4 @@
-with open ('input.txt', 'r') as f:
-    C=list(f.readline())
+with open ('pr5.txt', 'r') as f:
+    C=f.readline()
 n=[i for i in C if i in 'aeiouAEIOU']
 print('Numarul vocalelor din fisier este', len(n))
