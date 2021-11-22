db970f6d851a8062cc72a59acb0ca52c443619bb
elenamoglan <moglan.elena@gmail.com>
2021-11-22 10:09:52 +0200

    Update Problema 5 p 77.py

--- Problema 5 p 77.py
+++ Problema 5 p 77.py
@@ -1,4 +1,6 @@
-with open ('pr5.txt', 'r') as f:
+with open ('c:/Users/User/Desktop/input.txt', 'r') as f:
     C=f.readline()
 n=[i for i in C if i in 'aeiouAEIOU']
-print('Numarul vocalelor din fisier este', len(n))
+with open ('c:/Users/User/Desktop/output.txt', 'w') as f:
+    f.write('Vocalele: '+str(n))
+print(f'Vocalele sunt {n}, iar numarul lor este {len(n)}')
